2342018e4b6d83f953d05911b247ed657accf23e
ThomasTadesse <328704@student.mboutrecht.nl>
2025-03-06 16:12:56 +0100

    SP pending

--- Magazijn_Jamin/database/migrations/2025_02_17_225535_create_stored_procedure_allergenen.php
+++ Magazijn_Jamin/database/migrations/2025_02_17_225535_create_stored_procedure_allergenen.php
@@ -12,41 +12,10 @@ return new class extends Migration
     public function up(): void
     {
         DB::unprepared('
-        DROP PROCEDURE IF EXISTS GetAllergenen;
-        CREATE PROCEDURE GetAllergenen()
-        BEGIN
-            SELECT 
-                a.id AS Id,
-                p.Naam AS ProductNaam,
-                a.Naam AS AllergeenNaam,
-                a.Omschrijving,
-                m.AantalAanwezig
-            FROM Allergeen a
-            JOIN ProductPerAllergeen pa ON a.Id = pa.AllergeenId
-            JOIN Product p ON pa.ProductId = p.Id
-            LEFT JOIN Magazijn m ON p.Id = m.ProductId
-            WHERE a.IsActief = 1 AND p.IsActief = 1
-            ORDER BY ProductNaam ASC;
-        END');
+       ');
 
         DB::unprepared('
-        DROP PROCEDURE IF EXISTS GetLeverancierByProductId;
-        CREATE PROCEDURE GetLeverancierByProductId(IN productId INT)
-        BEGIN
-            SELECT 
-                l.id AS Id,
-                l.Naam AS LeverancierNaam,
-                l.Contactpersoon,
-                l.Mobiel,
-                c.Stad,
-                c.Straat,
-                c.Huisnummer
-            FROM Leverancier l
-            JOIN Contact c ON l.ContactId = c.Id
-            JOIN ProductPerLeverancier ppl ON l.Id = ppl.LeverancierId
-            WHERE ppl.ProductId = productId
-            AND ppl.IsActief = 1;
-        END');
+       ');
     }
 
     /**
@@ -54,7 +23,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        DB::unprepared('DROP PROCEDURE IF EXISTS GetAllergenen');
-        DB::unprepared('DROP PROCEDURE IF EXISTS GetLeverancierByProductId');
+        DB::unprepared('DROP PROCEDURE IF EXISTS ');
+        DB::unprepared('DROP PROCEDURE IF EXISTS ');
     }
 };
